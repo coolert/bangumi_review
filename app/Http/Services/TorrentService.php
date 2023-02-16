4804eaca968321dc4d64aeece721489d7a14993f
keith <1033610335@qq.com>
2023-02-16 10:47:13 +0800

    torrent list bug fixed

--- app/Http/Services/TorrentService.php
+++ app/Http/Services/TorrentService.php
@@ -4,17 +4,31 @@ namespace App\Http\Services;
 
 class TorrentService extends BaseService
 {
-    private const RSS_URLS = [
+    public const RSS_URLS = [
         //动漫花园
-        'anima_flower' => 'https://dmhy.b168.net/topics/rss/rss.xml?keyword=',
+        'anima_flower' => 'https://dmhy.b168.net/topics/rss/rss.xml?keyword={keywords}',
         //萌番组
-        'anima_group' => 'https://bangumi.moe/rss/search/',
+        'anima_group' => 'https://bangumi.moe/rss/search/{keywords}',
         //蜜柑
-        'mikan' => 'https://mikanani.me/RSS/Search?searchstr=',
+        'mikan' => 'https://mikanani.me/RSS/Search?searchstr={keywords}',
     ];
     public function __construct()
     {
         //
+    }
+
+    /**
+     * 获取下载信息列表
+     *
+     * @param $key_words
+     * @param $url
+     *
+     * @return array
+     */
+    public function get_torrent_list($key_words, $url)
+    {
+        $path = str_replace('{keywords}', urlencode($key_words), $url);
+        return $this->analysis_rss($path);
     }
 
     /**
@@ -46,15 +60,15 @@ class TorrentService extends BaseService
         foreach ($values as $v) {
             $tag = $v["tag"];
             $type = $v["type"];
-            if ($tag == "item" && $type == "open") {
+            if ($tag == "ITEM" && $type == "open") {
                 $item_key++;
             }
             //仅读取 item 标签中的内容
             if ($item_key >= 0) {
-                if ($tag == "title") {
+                if ($tag == "TITLE") {
                     $list[$item_key]['title'] = $v["value"];
                 }
-                if ($tag == "enclosure") {
+                if ($tag == "ENCLOSURE") {
                     $list[$item_key]['url'] = $v['attributes']['URL'];
                 }
             }
